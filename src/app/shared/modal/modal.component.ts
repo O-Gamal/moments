b7721a01ba43c1dd933872847061cd4758503be8
omar <o.gamal.abdulzaher@gmail.com>
2022-12-28 13:56:54 +0200

    implement modal componat

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+})
+export class ModalComponent {
+  @Input() modalId = '';
+  constructor(public modal: ModalService) {}
+
+  closeModal() {
+    this.modal.toggleModal(this.modalId);
+  }
+}
