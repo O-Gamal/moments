356717fb065b4e13299e63398374c530bf31492a
omar <o.gamal.abdulzaher@gmail.com>
2022-12-30 00:15:48 +0200

    implement register form

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
-  declarations: [ModalComponent],
-  imports: [CommonModule],
-  exports: [ModalComponent],
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+    AlertComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+    AlertComponent,
+  ],
 })
 export class SharedModule {}
---
+++ src/app/user/register/register.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent {
+  showAlert = false;
+  alertMessage = '';
+  alertType = '';
+
+  registerForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    age: new FormControl('', [
+      Validators.required,
+      Validators.min(12),
+      Validators.max(120),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
+      ),
+    ]),
+    confirm_password: new FormControl('', [Validators.required]),
+    phone_number: new FormControl(''),
+  });
+
+  register() {
+    this.showAlert = true;
+    this.alertMessage = 'Please wait! your account is being created';
+    this.alertType = 'normal';
+  }
+}
