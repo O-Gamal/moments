3952b00950c4493ce7f83077a19e992e062dfd9d
omar <o.gamal.abdulzaher@gmail.com>
2022-12-31 19:20:24 +0200

    implement login form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  showAlert = false;
+  alertMessage = '';
+  alertType = '';
 
+  credentials = {
+    email: '',
+    password: '',
+  };
+
+  login() {
+    this.showAlert = true;
+    this.alertMessage = 'Please wait! your account is being validating';
+    this.alertType = 'normal';
+  }
 }
