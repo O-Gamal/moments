07de1ba3277267a3912bec898061edc0fb6b9c5e
omar <o.gamal.abdulzaher@gmail.com>
2023-01-04 13:38:58 +0200

    implement additional registeration form validation

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import User from 'src/app/models/user.model';
 import { AuthService } from 'src/app/services/auth.service';
 import { AlertType } from 'src/app/shared/alert/alert.component';
+import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +12,7 @@ import { AlertType } from 'src/app/shared/alert/alert.component';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private emailTaken: EmailTaken) {}
 
   inSubmission = false;
 
@@ -18,23 +20,29 @@ export class RegisterComponent {
   alertMessage = '';
   alertType: AlertType = '';
 
-  registerForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    age: new FormControl<number | null>(null, [
-      Validators.required,
-      Validators.min(12),
-      Validators.max(120),
-    ]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.pattern(
-        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
+  registerForm = new FormGroup(
+    {
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl(
+        '',
+        [Validators.required, Validators.email],
+        [this.emailTaken.validate]
       ),
-    ]),
-    confirm_password: new FormControl('', [Validators.required]),
-    phone_number: new FormControl(''),
-  });
+      age: new FormControl<number | null>(null, [
+        Validators.required,
+        Validators.min(12),
+        Validators.max(120),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(
+          /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
+        ),
+      ]),
+      confirm_password: new FormControl('', [Validators.required]),
+    },
+    [RegisterValidators.match('password', 'confirm_password')]
+  );
 
   async register() {
     this.showAlert = true;
