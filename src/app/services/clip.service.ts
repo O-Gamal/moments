e84e4be30b5889ac62d4613aafa68e9dc84d2e7a
omar <o.gamal.abdulzaher@gmail.com>
2023-01-11 08:39:49 +0200

    Implement edit, delete, sort clip functionality

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -3,8 +3,12 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
   DocumentReference,
+  QuerySnapshot,
 } from '@angular/fire/compat/firestore/';
 import Clip from '../models/clip.model';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { map, of, switchMap, BehaviorSubject, combineLatest } from 'rxjs';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @Injectable({
   providedIn: 'root',
@@ -12,11 +16,43 @@ import Clip from '../models/clip.model';
 export class ClipService {
   public clipsCollection: AngularFirestoreCollection<Clip>;
 
-  constructor(private db: AngularFirestore) {
+  constructor(
+    private db: AngularFirestore,
+    private auth: AngularFireAuth,
+    private storage: AngularFireStorage
+  ) {
     this.clipsCollection = db.collection('clips');
   }
 
   createClip(data: Clip): Promise<DocumentReference<Clip>> {
     return this.clipsCollection.add(data);
+  }
+
+  getUserClips(sort$: BehaviorSubject<string>) {
+    return combineLatest([this.auth.user, sort$]).pipe(
+      switchMap((values) => {
+        const [user, sort] = values;
+        if (!user) {
+          return of([]);
+        }
+
+        const query = this.clipsCollection.ref
+          .where('uid', '==', user.uid)
+          .orderBy('timestamp', sort === '1' ? 'desc' : 'asc');
+        return query.get();
+      }),
+      map((snapShot) => (snapShot as QuerySnapshot<Clip>).docs)
+    );
+  }
+
+  updateClip(id: string, title: string) {
+    return this.clipsCollection.doc(id).update({
+      title,
+    });
+  }
+
+  async deleteClip(clip: Clip) {
+    await this.storage.ref(`clips/${clip.fileName}`).delete();
+    await this.clipsCollection.doc(clip.docID).delete();
   }
 }
--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import Clip from 'src/app/models/clip.model';
+import { ClipService } from 'src/app/services/clip.service';
+import { ModalService } from 'src/app/services/modal.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-manage',
@@ -9,12 +12,34 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 })
 export class ManageComponent implements OnInit {
   videoOrder = '1';
+  userClips: Clip[] = [];
+  activeClip: Clip | null = null;
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  sort$: BehaviorSubject<string>;
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private clipService: ClipService,
+    private modal: ModalService
+  ) {
+    this.sort$ = new BehaviorSubject<string>(this.videoOrder);
+  }
+
+  ngOnInit() {
     this.route.queryParams.subscribe((params: Params) => {
       this.videoOrder = params['sort'] || '1';
+      this.sort$.next(this.videoOrder);
+    });
+
+    this.clipService.getUserClips(this.sort$).subscribe((docs) => {
+      this.userClips = [];
+      docs.forEach((doc) => {
+        this.userClips.push({
+          docID: doc.id,
+          ...doc.data(),
+        });
+      });
     });
   }
 
@@ -25,6 +50,30 @@ export class ManageComponent implements OnInit {
       queryParams: {
         sort: value,
       },
+    });
+  }
+
+  openModal(e: Event, clip: Clip) {
+    e.preventDefault();
+    this.activeClip = clip;
+
+    this.modal.toggleModal('editClip');
+  }
+
+  update(clip: Clip) {
+    this.userClips.forEach((element, index) => {
+      if (clip.docID === element.docID) {
+        this.userClips[index].title = clip.title;
+      }
+    });
+  }
+
+  deleteClip(e: Event, clip: Clip) {
+    e.preventDefault();
+    this.clipService.deleteClip(clip);
+
+    this.userClips = this.userClips.filter((element) => {
+      return element.docID !== clip.docID;
     });
   }
 }
