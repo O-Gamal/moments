19bedac66026c1149ad14bc6a5259614e602bf45
omar <o.gamal.abdulzaher@gmail.com>
2023-01-19 13:59:44 +0200

    Implement mobile layout

--- src/app/nav-link/nav-link.component.ts
+++ src/app/nav-link/nav-link.component.ts
@@ -14,6 +14,7 @@ import { Router } from '@angular/router';
 })
 export class NavLinkComponent {
   @Input() route = '';
+  @Input() mobileSize = false;
 
   // constructor(private router: Router) {}
 
